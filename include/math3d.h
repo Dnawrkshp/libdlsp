953a88dafb60438a98b020fcb3886978e0c7fc1e
Daniel Gerendasy <Dnawrkshp@users.noreply.github.com>
2023-11-29 19:32:19 -0800

    Add pad button bitmasks Align vectors

--- include/math3d.h
+++ include/math3d.h
@@ -5,8 +5,8 @@
 
 //--------------------------------------------------------
 typedef s128 vec4;
-typedef float vec4f[4];
-typedef float vec2f[2];
+typedef float vec4f[4]  __attribute__((__aligned__(16)));;
+typedef float vec2f[2]  __attribute__((__aligned__(8)));;
 
 //--------------------------------------------------------
 void vector_write(void* output, vec4 input0);
