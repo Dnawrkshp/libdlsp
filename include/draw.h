a7b10522a7110fe27dd833ef4cbf7d32a891b613
Daniel Gerendasy <Dnawrkshp@users.noreply.github.com>
2023-11-29 20:19:10 -0800

    Add DrawWidget2DFlatPrim

--- include/draw.h
+++ include/draw.h
@@ -8,11 +8,14 @@
 
 //--------------------------------------------------------
 typedef void (*DrawQuad_f)(vec2f *vPoints, struct DrawParams *pParams);
+typedef void (*DrawWidget2DFlatPrim_f)(struct Widget2D* widget, u32 rgba, int prim_type);
 
 //--------------------------------------------------------
 DECLARE_OVERLAY_LOOKUP(DrawQuad_lookup, DrawQuad_f);
+DECLARE_OVERLAY_LOOKUP(DrawWidget2DFlatPrim_asm_lookup, DrawWidget2DFlatPrim_f);
 
 //--------------------------------------------------------
 void DrawQuad(vec2f *vPoints, struct DrawParams *pParams);
+void DrawWidget2DFlatPrim(struct Widget2D* widget, u32 rgba, int prim_type);
 
 #endif // _LIBDLSP_DRAW_H_
--- src/draw.c
+++ src/draw.c
@@ -12,3 +12,16 @@ void DrawQuad(vec2f *vPoints, struct DrawParams *pParams)
 
   drawQuadFunc(vPoints, pParams);
 }
+
+//--------------------------------------------------------
+BEGIN_OVERLAY_LOOKUP(DrawWidget2DFlatPrim_asm_lookup, DrawWidget2DFlatPrim_f)
+  OVERLAY_LOOKUP_ENTRY_NTSC(MAIN_MENU, 0x004ee5c0)
+END_OVERLAY_LOOKUP(DrawWidget2DFlatPrim_asm_lookup)
+
+void DrawWidget2DFlatPrim(struct Widget2D* widget, u32 rgba, int prim_type)
+{
+  DrawWidget2DFlatPrim_f drawWidget2DFlatPrimFunc = (DrawWidget2DFlatPrim_f)GetOverlayAddress((void**)&DrawWidget2DFlatPrim_asm_lookup);
+  if (!drawWidget2DFlatPrimFunc) return;
+
+  drawWidget2DFlatPrimFunc(widget, rgba, prim_type);
+}
